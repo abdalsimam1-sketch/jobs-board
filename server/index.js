adcb37e4c6acc4edfc349a54a6f7200ed106fe9e
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-06-10 17:48:28 +0100

    chore : starting express server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const app = express();
+const pool = require("./config/connectDB");
+
+const errorHandler = require("./middleware/errorHandler");
+const routeNotFound = require("./middleware/notFound");
+
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+
+//security
+app.use(cors());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
+
+//routes
+//errors
+app.use(routeNotFound);
+app.use(errorHandler);
+
+//start server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}....`);
+});
