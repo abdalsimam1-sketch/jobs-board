9501a1b5e679dd8bb4d57332fa65e6bca3cecbcc
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-06-14 11:42:24 +0100

    refactor : fix quotes from double to single

--- server/db/migrations/0004_add_contriants_to_applications.sql
+++ server/db/migrations/0004_add_contriants_to_applications.sql
@@ -1,4 +1,4 @@
 alter table applications alter column status set not null;
-alter table applications after column status set default "pending";
+alter table applications after column status set default 'pending';
 alter table  applications alter column seeker_id set not null;
 alter table applications alter column job_id set not null;
