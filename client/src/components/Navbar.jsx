758e9d0a5cc51cd822d50fd10951b93cb4824028
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-06-16 12:57:21 +0100

    feat : navbar with 2 roles

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { logout } from "../utils/logout";
+import { useNavigate, Link } from "react-router-dom";
+
+export const Navbar = ({ role }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="d-flex justify-content-between align-items-center border-bottom">
+      <div>
+        <h1>JobBoard</h1>
+      </div>
+      {role === "seeker" && (
+        <div className="d-flex gap-3">
+          <span className="cursor-pointer">Home</span>
+          <span className="cursor-pointer">My applications</span>
+          <span className="cursor-pointer" onClick={() => logout(navigate)}>
+            Logout
+          </span>
+        </div>
+      )}
+      {role === "poster" && (
+        <div className="d-flex gap-3">
+          <span className="cursor-pointer">Dashboard</span>
+          <span className="cursor-pointer">My Jobs</span>
+          <span className="cursor-pointer">Application</span>
+          <span className="cursor-pointer" onClick={() => logout(navigate)}>
+            Logout
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
